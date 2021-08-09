9c2c94f2665d4f09b7326373428ed35863572e0b
ajisetiaji101 <ajisetiaji101@gmail.com>
2021-08-09 13:11:47 +0700

    baru

--- components/Cart/Cart.js
+++ components/Cart/Cart.js
@@ -4,7 +4,7 @@ export default function Cart() {
     return (
         <div className="flex flex-col items-center justify-center min-h-screen py-2">
             <h1>Your Shopping Cart</h1>
-            <p>Name</p>
+            <p>Bye</p>
         </div>
     )
 }
